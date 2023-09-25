0695e28dce06164aefd16d5b4ad5b41e8a7522cc
ankitchoudhary8120@gmail.com <ankitchoudhary8120@gmail.com>
2023-09-25 13:37:15 +0530

    installation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Header from "./Header";
 
 
 function App() {
   return (
     <div className="App">
-      <h1>We are developing gmail clone with react js</h1>
+      <Header />
     </div>
   );
 }
